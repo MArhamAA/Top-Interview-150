46bd1dac79492341aa25fecc8e477e2da0ee96ae
Md::Arham <arham511624@gmail.com>
2024-01-16 21:50:14 +0600

    Solved prob-002

--- Problems/Prob-002/prob_002.cpp
+++ Problems/Prob-002/prob_002.cpp
@@ -1,1 +1,25 @@
-ECHO is on.
+// BISMILLAH
+
+#include <bits/stdc++.h>
+
+int removeElement(std::vector<int>& nums, int val) {
+    std::vector<int> ans;
+    for (auto i : nums) {
+        if (i != val) ans.push_back(i);
+    }
+
+    nums = ans;
+    return (int)ans.size();
+}
+
+signed main() {
+    //
+}
+
+/*
+
+https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
+
+Iteration over array
+
+*/
