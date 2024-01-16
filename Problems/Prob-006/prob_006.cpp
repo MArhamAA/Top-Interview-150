2cac7d051201952c8b5436ba23971dbbb80d2fee
Md::Arham <arham511624@gmail.com>
2024-01-16 22:02:13 +0600

    Solved prob-006

--- Problems/Prob-006/prob_006.cpp
+++ Problems/Prob-006/prob_006.cpp
@@ -1,1 +1,36 @@
-ECHO is on.
+// BISMILLAH
+
+#include <bits/stdc++.h>
+
+int POS(int i, int j, int N) {
+    return (i+j)%N;
+}
+void rotate(std::vector<int>& nums, int k) {
+    int N = (int)nums.size();
+    k %= N;
+    if (k == 0) return;
+    else {
+        std::deque<int> d;
+        for (auto i : nums) d.push_back(i);
+        for (int i = 0; i < k; i++) {
+            d.push_front(d.back());
+            d.pop_back();
+        }
+        nums.clear();
+        for (auto i : d) nums.push_back(i);
+    }
+}
+
+signed main() {
+	//
+}
+
+/*
+
+https://leetcode.com/problems/rotate-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+Deque
+
+In-place
+
+*/
