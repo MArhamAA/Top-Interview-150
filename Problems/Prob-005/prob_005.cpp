d4535a64d861c02aa16e18777ab10706247104b1
Md::Arham <arham511624@gmail.com>
2024-01-16 21:57:27 +0600

    Solved prob-005

--- Problems/Prob-005/prob_005.cpp
+++ Problems/Prob-005/prob_005.cpp
@@ -1,1 +1,32 @@
-ECHO is on.
+// BISMILLAH
+
+#include <bits/stdc++.h>
+
+int majorityElement(std::vector<int>& nums) {
+    std::unordered_map<int,int> count;
+    for (auto i : nums) {
+        count[i]++;
+    }
+    int ans, tot = 0;
+    for (auto i : count) {
+        if (i.second > tot) {
+            tot = i.second;
+            ans = i.first;
+        }
+    }
+    return ans;
+}
+
+signed main() {
+	//
+}
+
+/*
+
+https://leetcode.com/problems/majority-element/?envType=study-plan-v2&envId=top-interview-150
+
+Hashmap
+
+Boyer–Moore majority vote algorithm
+
+*/
