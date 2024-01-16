84d051f8a2aef9bec201b98d22df5fcf745893c2
Md::Arham <arham511624@gmail.com>
2024-01-16 21:52:30 +0600

    Solved prob-003

--- Problems/Prob-003/prob_003.cpp
+++ Problems/Prob-003/prob_003.cpp
@@ -1,1 +1,30 @@
-ECHO is on.
+// BISMILLAH
+
+#include <bits/stdc++.h>
+
+int removeDuplicates(std::vector<int>& nums) {
+    std::vector<int> ans;
+    std::unordered_map<int,bool> used;
+
+    for (int i : nums) {
+        if (used.find(i) == used.end()) {
+            ans.push_back(i);
+        }
+        used[i] = true;
+    }
+
+    nums = ans;
+    return (int)ans.size();
+}
+
+signed main() {
+	//
+}
+
+/*
+
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+Iteration over array & hashmap
+
+*/
